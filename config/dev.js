acf8cb319b1bc8739e08b7e56edab49f63641ded
DeftCorgi <toanau123@gmail.com>
2018-09-28 12:03:32 +1000

    added keys

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+// development keys
+// do NOT commit this file to github, secret information
+const keys = {
+  googleClientId: '',
+  googleClientSecret: '',
+  googleProjectId: '',
+  gcsBucketName: '',
+  dbString: '',
+  cookieKey: 'holaamiga'
+};
+
+module.exports = keys;
---
+++ config/prod.js
@@ -0,0 +1,11 @@
+// production keys
+// keys shold be saved in the environment NOT in this file
+const keys = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  gcsBucketName: process.env.GCS_BUCKET_NAME,
+  dbString: process.env.DB_STRING,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+module.exports = keys;
